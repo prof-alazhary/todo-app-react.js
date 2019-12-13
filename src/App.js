6e795395240dc48be9e21f7afb8b5f3f893e6971
Mohamed Alazhary <mohamed.elazhary@vodafone.com>
2019-12-13 15:28:47 +0200

    npm eject

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../public/assets/logo.svg';
+//import logo from '../public/assets/logo.svg';
 import TodoContainer from './components/TodoContainer';
-
+window.React1 = React;
+const path = require('path');
 class App extends Component {
+    constructor(props){
+        super(props);
+
+    }
     state = { users: [] };
 
     componentDidMount() {
@@ -21,11 +26,13 @@ class App extends Component {
     }
 
     render() {
+        require('react-dom');
+        window.React1 = require('react');
         console.log('state-->', this.state);
         return (
             <div className="App">
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
                     <h2>Welcome to React</h2>
                 </div>
                 <p className="App-intro">
--- src/components/TodoRow.js
+++ src/components/TodoRow.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Checkbox } from 'cit-retail10-react-common';
 
-class TodoRow extends React.Component {   
+class TodoRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false
+        }
+    }
+
     handleChecke(e) {
-        if(e.target.nodeName == "LI"){
+        if (e.target.nodeName == "LI") {
             this.props.onTodoChecked(e.target.id);
         }
     }
-    handleRemove(id,e){
+    handleRemove(id, e) {
         this.props.onTodoRemoved(id)
     }
     render() {
+        const { checked } = this.state || {};
         return (
             <li
                 onClick={this.handleChecke.bind(this)}
@@ -17,6 +26,27 @@ class TodoRow extends React.Component {
                 id={this.props.id}
             >
                 {this.props.title}
+                <Checkbox
+                    inputProps={{ 'data-test': 'compareCheckBox', 'aria-label': 'Add to comparison checkbox' }}
+                    className="addToCompare-checkbox"
+                    checked={checked}
+                    disabled={false}
+                    onChange={d => {
+                        this.setState({ checked: !checked })
+                    }}
+     
+                            value="addedToCompare"
+                            label="Checkbox"
+                    style={{ padding: 0, marginRight: 10, marginLeft: 1 }}
+                />
+                {/* <Image
+                                id="product-view-img"
+                                alt={"hamada"}
+                                width={92}
+                                height={92}
+                                style={{ objectFit: "contain", minWidth: 92 }}
+                                src={"https://via.placeholder.com/150"}
+                    /> */}
                 <span onClick={this.handleRemove.bind(this, this.props.id)} className="close">×</span>
             </li>
         );
